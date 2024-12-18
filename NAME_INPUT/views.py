31739b57f69c1fd0d3aa5fd46763d85b4e1ed17f
Sammy <sammy.liaop@gmail.com>
2024-12-18 01:13:57 -0500

    view.py added

--- NAME_INPUT/views.py
+++ NAME_INPUT/views.py
@@ -1,13 +1,7 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .serializers import NameSerializer
 from .models import Name
 
-
-class NameList(generics.ListCreateAPIView):
-   queryset = Name.objects.all().order_by('id')
-   serializer_class = NameSerializer
-
-
-class NameDetail(generics.RetrieveUpdateDestroyAPIView):
-   queryset = Name.objects.all().order_by('id')
-   serializer_class = NameSerializer
+class NameViewSet(viewsets.ModelViewSet):
+    queryset = Name.objects.all()
+    serializer_class = NameSerializer
