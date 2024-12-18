2a553aefb35974160fa559153090792dcac39d95
Sammy <sammy.liaop@gmail.com>
2024-12-18 01:12:24 -0500

    fixing

--- NAME_INPUT/urls.py
+++ NAME_INPUT/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 
 urlpatterns = [
-   path('api/nam', views.NameList.as_view(), name='name_list'),
+   path('api/name', views.NameList.as_view(), name='name_list'),
    path('api/name/<int:pk>', views.nameDetail.as_view(), name='name_detail'),
 ]
